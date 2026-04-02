8baa7bbd3b1340544b66e4fa9dfb5c69de95d57a
CreaperGames1 <tomas.tersago@outlook.com>
2026-04-02 14:02:08 +0200

    s

--- index.js
+++
@@ -1,5 +0,0 @@
-const audio = document.getElementById('audio');
-audio.play().catch(() => {
-  document.addEventListener('click', () => audio.play(), { once: true });
-  document.addEventListener('touchstart', () => audio.play(), { once: true });
-});
